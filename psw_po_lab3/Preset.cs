650ab5757d93e0717100e7b11c4e3186f9d13a4c
lukasz_kusyk_psw <lukifrancuz@gmail.com>
2021-10-26 20:09:43 +0200

    add preset class

---
+++ psw_po_lab3/Preset.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psw_po_lab3
+{
+    class Preset
+    {
+        //Wiem że można inaczej, ale świadomie nie korzystam z automatycznej właśniwości Name.
+        private string name;
+        private double value;
+
+        public string Name
+        {
+            get => name;
+            private set => name = value;
+        }
+
+        public double Value
+        {
+            get => value;
+            private set
+            {
+                if (value > 100)
+                    this.value = 100;
+                else if (value < 0)
+                    this.value = 0;
+                else
+                    this.value = value;
+            }
+        }
+
+        public Preset(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
