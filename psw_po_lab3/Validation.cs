81341e65520fc0b3fa6b25bfa0bb2fc7cf387f0e
lukasz_kusyk_psw <lukifrancuz@gmail.com>
2021-10-26 20:40:45 +0200

    add validator class

---
+++ psw_po_lab3/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace psw_po_lab3
+{
+    class Validation
+    {
+        public static void DoubleValidation(TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9|.]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        public static void UintValidation(TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
